7a1ab60bdb63f2732baac801efa28000220fa4bd
Omid <omidshadpour25@gmail.com>
2026-06-12 23:17:40 +0330

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
+
 uvicorn main:app --host 0.0.0.0 --port 8000 &
+
+sleep 5
+
+
+curl http://localhost:8000/ || echo "FastAPI NOT running!"
+
 
 streamlit run app/ui/streamlit_app.py --server.port 7860 --server.address 0.0.0.0
