28cd508d8f9315b905e686cd36308d48f8b1b0c7
Omid <omidshadpour25@gmail.com>
2026-06-12 23:48:32 +0330

    Run FastAPI on port 7860

--- start.sh
+++ start.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-
-# FastAPI روی پورت 8000
-uvicorn main:app --host 0.0.0.0 --port 8000 &
-
-sleep 30
-
-# Streamlit روی پورت 7860 با root path
-streamlit run app/ui/streamlit_app.py \
-  --server.port 7860 \
-  --server.address 0.0.0.0 \
-  --server.baseUrlPath ""
+uvicorn main:app --host 0.0.0.0 --port 7860
